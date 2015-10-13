7d1a910f924d469f67b1d229966bd583d1b04b64
Adam Ralph <adam@adamralph.com>
2015-10-13 21:35:21 +0200

    add more parallelism to Program

--- src/Uranium.Console/Program.cs
+++ src/Uranium.Console/Program.cs
@@ -1,7 +1,6 @@
 ﻿namespace Uranium.Console
 {
     using System;
-    using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
     using System.Linq;
@@ -25,96 +24,64 @@
                 Environment.GetEnvironmentVariable("OCTOKIT_GITHUBUSERNAME"),
                 Environment.GetEnvironmentVariable("OCTOKIT_GITHUBPASSWORD"));
 
-            var groups = await new CrappyCommitterGroupService("groups.txt", "Particular").Get("Particular");
+            var committerGroups = await new CrappyCommitterGroupService("groups.txt", "Particular").Get("Particular");
 
             var repositoryService = new RepositoryService(client);
-            var repositories = (await Task.WhenAll(groups
+            var repositories = (await Task.WhenAll(committerGroups
                     .SelectMany(@group => @group.RepositoryList.Select(id => id.Owner))
                     .Distinct()
-                    .Select(org => repositoryService.Get(org))))
-                .SelectMany(_ => _);
+                    .Select(owner => repositoryService.Get(owner))))
+                .SelectMany(_ => _)
+                .ToList();
 
-            foreach (var repo in repositories
-                .Where(repo => !groups.Any(group => group.RepositoryList.Contains(repo.Id)))
-                .OrderBy(_ => _))
+            var ungroupedRepositories = repositories
+                .Where(repository => !committerGroups.Any(group => group.RepositoryList.Contains(repository.Id)));
+
+            foreach (var repo in ungroupedRepositories.OrderBy(_ => _))
             {
                 ColorConsole.WriteLine(
                     "* **".White(), $"Repo '{repo.Id.Owner}/{repo.Id.Name}' is not grouped!".Red(), "**".White());
             }
 
-            var groupLoginContributions = new List<Contribution>();
             var commitService = new CommitService(client);
-            foreach (var group in groups)
-            {
-                ColorConsole.WriteLine();
-                ColorConsole.WriteLine();
-                ColorConsole.WriteLine("# ".White(), group.Name.Yellow());
-                ColorConsole.WriteLine("### Repos".White());
-                var contributions = new Dictionary<string, double>();
-                foreach (var repo in group.RepositoryList)
+            var contributions = (await Task.WhenAll(committerGroups.SelectMany(group => group.RepositoryList.Select(id =>
                 {
-                    ColorConsole.WriteLine("* ".White(), repo.Name.Green());
-
-                    IReadOnlyList<Commit> commits;
-                    try
+                    ColorConsole.WriteLine($"Getting commits for \"#{id.Owner}/#{id.Name}\"...".Green());
+                    return commitService.Get(id.Owner, id.Name).ContinueWith(task =>
                     {
-                        commits = await commitService.Get(repo.Owner, repo.Name);
-                    }
-                    catch (Exception ex)
-                    {
-                        ColorConsole.WriteLine(
-                            "*".White(),
-                            $"Failed to get commits for '{repo.Owner}/{repo.Name}'.".Red(),
-                            " ",
-                            ex.Message.Yellow(),
-                            "*".White());
-
-                        continue;
-                    }
-
-                    foreach (var commit in commits.Where(commit => commit.Committer != null))
-                    {
-                        var age = Period.Between(
-                            commit.Committed.LocalDateTime,
-                            OffsetDateTime.FromDateTimeOffset(DateTimeOffset.UtcNow).LocalDateTime);
-
-                        var value = 1 / Math.Pow(2, 2 * age.Days / 365.25d);
-
-                        double sum;
-                        if (!contributions.TryGetValue(commit.Committer, out sum))
+                        if (task.Exception != null)
                         {
-                            contributions.Add(commit.Committer, value);
+                            ColorConsole.WriteLine($"Failed to getting commits for \"#{id.Owner}/#{id.Name}\". #{task.Exception.InnerException.Message}".Red());
+                            return Enumerable.Empty<Commit>();
                         }
-                        else
-                        {
-                            contributions[commit.Committer] = sum + value;
-                        }
-                    }
-                }
 
-                groupLoginContributions.AddRange(contributions
-                    .OrderByDescending(contribution => contribution.Value)
-                    .Select(contribution =>
-                        new Contribution { Group = group.Name, Login = contribution.Key, Score = contribution.Value }));
-            }
+                        Console.WriteLine($"Got commits for \"#{id.Owner}/#{id.Name}\"".Green());
+                        return task.Result;
+                    });
+                }))))
+                .SelectMany(_ => _)
+                .Select(commit => new { Login = commit.Committer, commit.Repository, Score = Score(commit) })
+                .GroupBy(contribution => new { Group = committerGroups.First(group => group.RepositoryList.Contains(contribution.Repository)).Name, contribution.Login })
+                .Select(g => new Contribution { Group = g.Key.Group, Login = g.Key.Login, Score = g.Sum(contribution => contribution.Score) })
+                .ToList();
 
             using (var writer = new StreamWriter("matrix.txt", false))
             {
                 writer.Write("Login/Group");
-                foreach (var group in groupLoginContributions.Select(contribution => contribution.Group).Distinct())
+                foreach (var group in contributions.Select(contribution => contribution.Group).Distinct())
                 {
                     writer.Write("\t" + group);
                 }
 
                 writer.WriteLine();
 
-                foreach (var login in groupLoginContributions.Select(contribution => contribution.Login).Distinct())
+                foreach (var login in contributions.Select(contribution => contribution.Login).Distinct())
                 {
                     writer.Write(login);
-                    foreach (var group in groupLoginContributions.Select(contribution => contribution.Group).Distinct())
+                    foreach (var group in contributions.Select(contribution => contribution.Group).Distinct())
                     {
                         var contribution =
-                            groupLoginContributions.SingleOrDefault(
+                            contributions.SingleOrDefault(
                                 candidate => candidate.Group == group && candidate.Login == login);
 
                         writer.Write(
@@ -125,6 +92,15 @@
                     writer.WriteLine();
                 }
             }
+        }
+
+        private static double Score(Commit commit)
+        {
+            var age = Period.Between(
+                commit.Committed.LocalDateTime,
+                OffsetDateTime.FromDateTimeOffset(DateTimeOffset.UtcNow).LocalDateTime);
+
+            return 1 / Math.Pow(2, 2 * age.Days / 365.25d);
         }
 
         private class Contribution
