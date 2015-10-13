a571a0e3ee6a40cb3e97d54c5ccddbed50081888
Adam Ralph <adam@adamralph.com>
2015-10-13 22:30:55 +0200

    refactor: whitespace

--- src/Uranium.Model/ContributionService.cs
+++ src/Uranium.Model/ContributionService.cs
@@ -14,25 +14,29 @@ namespace Uranium.Model
             Guard.AgainstNullArgument(nameof(committerGroups), committerGroups);
             Guard.AgainstNullArgument(nameof(commitService), commitService);
 
-            return (await Task.WhenAll(committerGroups.SelectMany(group => @group.RepositoryList.Select(id =>
-            {
-                Console.WriteLine($"Getting commits for \"#{id.Owner}/#{id.Name}\"...");
-                return commitService.Get(id.Owner, id.Name).ContinueWith(task =>
-                {
-                    if (task.Exception != null)
+            return
+                (await Task.WhenAll(committerGroups.SelectMany(group => @group.RepositoryList.Select(id =>
                     {
-                        Console.WriteLine($"Failed to getting commits for \"#{id.Owner}/#{id.Name}\". #{task.Exception.InnerException.Message}");
-                        return Enumerable.Empty<Commit>();
-                    }
+                        Console.WriteLine($"Getting commits for \"#{id.Owner}/#{id.Name}\"...");
+                        return commitService.Get(id.Owner, id.Name).ContinueWith(task =>
+                        {
+                            if (task.Exception != null)
+                            {
+                                Console.WriteLine($"Failed to getting commits for \"#{id.Owner}/#{id.Name}\". #{task.Exception.InnerException.Message}");
+                                return Enumerable.Empty<Commit>();
+                            }
 
-                    Console.WriteLine($"Got commits for \"#{id.Owner}/#{id.Name}\"");
-                    return task.Result;
-                });
-            }))))
+                            Console.WriteLine($"Got commits for \"#{id.Owner}/#{id.Name}\"");
+                            return task.Result;
+                        });
+                    }))))
                 .SelectMany(_ => _)
                 .Select(commit => new { Login = commit.Committer, commit.Repository, Score = Score(commit) })
-                .GroupBy(contribution => new { Group = committerGroups
-                    .First(group => @group.RepositoryList.Contains(contribution.Repository)).Name, contribution.Login })
+                .GroupBy(contribution => new
+                {
+                    Group = committerGroups.First(group => @group.RepositoryList.Contains(contribution.Repository)).Name,
+                    contribution.Login
+                })
                 .Select(g => new Contribution(g.Key.Group, g.Key.Login, g.Sum(contribution => contribution.Score)))
                 .ToList();
         }
